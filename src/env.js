f660986413c3a9377340096ad779079032838255
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-10-02 18:55:41 -0700

    removed unecessary env variables and updated homepage

--- src/env.js
+++ src/env.js
@@ -1,6 +1,12 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-
+/**
+ * This file is compiled into a rollup config file when used with `create-t3-app`.
+ * Learn more at: https://create.t3.gg/en/usage/env-variables
+ * TLDR:
+ *  This files makes our environment variables type safe and consistent in all environments
+ *  If you want to add a new environment variable, you must add a validator for it in src/env.js, and then add the KV-pair in .env
+ */
 export const env = createEnv({
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app
@@ -20,10 +26,8 @@ export const env = createEnv({
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
+      process.env.VERCEL ? z.string() : z.string().url(),
     ),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
   },
 
   /**
@@ -44,8 +48,6 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
