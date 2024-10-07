da65695025dc2bee16bc536fd9abfb82ad49f3ec
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-10-06 22:15:17 -0700

    updated import alias for some files and updated changed the search bar to use shadcn components

--- src/components/client/Search.tsx
+++ src/components/client/Search.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export default function Search() {
   const router = useRouter();
@@ -9,23 +11,31 @@ export default function Search() {
 
   function handleSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
+
+    //provide type for e.target
     const target = e.target as typeof e.target & {
       searchTerm: { value: string };
     };
     const searchTerm = target.searchTerm.value;
+
+    //create a url friendly search params
     const params = new URLSearchParams(searchParams);
     if (searchTerm) {
       params.set("search", searchTerm);
     } else {
       params.delete("search");
     }
+    //navigate to search page with search params
     router.push(`/search/movie/?${params.toString()}`);
   }
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="searchTerm" required className="text-black" />
-        <button type="submit">Search</button>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-row items-center gap-3 rounded-md p-2"
+      >
+        <Input type="text" name="searchTerm" required className="text-black" />
+        <Button type="submit">Search</Button>
       </form>
     </>
   );
