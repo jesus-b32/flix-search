ff7feaa960df552dd60d47fb865151507c0b2064
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-10-16 19:42:00 -0700

    fixed prop key issue and display message when no streaming data is available

--- src/components/AvailabilityByProvider.tsx
+++ src/components/AvailabilityByProvider.tsx
@@ -5,19 +5,21 @@ import type {
 import SelectSearch from "./client/SelectSearch";
 import type { movieDetails } from "@/server/actions/movies/types";
 import type { tvDetails } from "@/server/actions/tv/types";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
 /**
  * flags from country-flag-icons repo
  * @see https://www.npmjs.com/package/country-flag-icons
  */
 
+/**
+ * Creates server component that displays all countries with availability of movie or TV show based on selected streaming provider
+ * @param {streamingProviderList} streamingProviderList - list of available streaming providers
+ * @param {movieDetails | tvDetails} details - movie or tv detail object
+ * @param {string} selectedStreamingProviderId - id of selected streaming provider
+ * @param {countryList} countries - list of available countries used in the TMDB API
+ * @returns {JSX.Element} - server component that displays country flags with the country name in a card
+ */
 export default function AvailabilityByProvider({
   streamingProviderList,
   details,
@@ -35,7 +37,6 @@ export default function AvailabilityByProvider({
     (provider) =>
       provider.provider_id.toString() === selectedStreamingProviderId,
   )?.provider_name;
-  let countryName: string;
 
   return (
     <div className="mt-6 flex w-10/12 flex-col items-center justify-center lg:w-10/12 lg:justify-start">
@@ -50,74 +51,49 @@ export default function AvailabilityByProvider({
       </div>
       <div className="mt-6 flex w-full flex-col">
         <h2 className="mb-3 text-2xl font-bold">
-          {`${title} is available on ${selectedStreamingProviderName} in the following countries:`}
+          {!watchProviders
+            ? null
+            : Object.keys(watchProviders).length === 0
+              ? "No Streaming data available for title"
+              : `${title} is available on ${selectedStreamingProviderName} in the following countries:`}
         </h2>
         <div className="grid grid-cols-1 justify-items-center gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-          {!watchProviders ? (
-            <p>No Streaming data available for title</p>
-          ) : (
-            Object.entries(watchProviders).map(([country, providerCountry]) => {
-              countryName =
-                countries.find((nation) => nation.iso_3166_1 === country)
-                  ?.native_name ?? "N/A";
-              return (
-                <>
-                  {Object.entries(providerCountry).map(
-                    ([providerType, providerDetails]) =>
-                      typeof providerDetails !== "string"
-                        ? providerDetails.map((provider) =>
-                            provider.provider_id.toString() ===
-                            selectedStreamingProviderId ? (
-                              <Card
-                                key={country}
-                                className="flex h-52 w-40 flex-col items-center"
-                              >
-                                <div className="flex justify-center">
-                                  <img
-                                    alt={countryName}
-                                    key={country}
-                                    src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${country}.svg`}
-                                    className="h-auto w-40 border border-black object-cover"
-                                  />
-                                </div>
-                                <div className="flex flex-col flex-wrap">
-                                  <CardHeader>
-                                    <CardTitle className="text-center text-xl">
-                                      {countryName}
-                                    </CardTitle>
-                                  </CardHeader>
-                                </div>
-                              </Card>
-                            ) : null,
-                          )
-                        : null,
-                  )}
-                </>
-              );
-            })
-          )}
+          {!watchProviders
+            ? null
+            : Object.entries(watchProviders).map(([country, providerCountry]) =>
+                Object.values(providerCountry).map((providerDetails) =>
+                  typeof providerDetails !== "string"
+                    ? providerDetails.map((provider) =>
+                        provider.provider_id.toString() ===
+                        selectedStreamingProviderId ? (
+                          <Card
+                            key={country}
+                            className="flex h-48 w-40 flex-col items-center"
+                          >
+                            <div className="flex justify-center">
+                              <img
+                                alt="country flag"
+                                src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${country}.svg`}
+                                className="h-auto w-40 border border-black object-cover"
+                              />
+                            </div>
+                            <div className="flex flex-col flex-wrap">
+                              <CardHeader className="px-1 pt-2">
+                                <CardTitle className="text-center text-xl">
+                                  {countries.find(
+                                    (nation) => nation.iso_3166_1 === country,
+                                  )?.native_name ?? "N/A"}
+                                </CardTitle>
+                              </CardHeader>
+                            </div>
+                          </Card>
+                        ) : null,
+                      )
+                    : null,
+                ),
+              )}
         </div>
       </div>
     </div>
   );
 }
-
-// Object.entries(watchProviders).map(([country, providerCountry]) =>
-//   Object.entries(providerCountry).map(
-//     ([providerType, providerDetails]) =>
-//       typeof providerDetails !== "string"
-//         ? providerDetails.map((provider) =>
-//             provider.provider_id.toString() ===
-//             selectedStreamingProviderId ? (
-//               <span key={provider.provider_id}>
-//                 {
-//                   countries.find(
-//                     (nation) => nation.iso_3166_1 === country,
-//                   )?.native_name
-//                 }
-//               </span>
-//             ) : null,
-//           )
-//         : null,
-//   ),
-// );
