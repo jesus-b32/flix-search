6e42ff22d4b1c885c9f326fec9cbe8b38934ee62
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-12-01 17:28:33 -0800

    updated not-found file

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { Frown } from "lucide-react";
 
 export default function NotFound() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-y-4">
-      <h2 className="text-2xl font-bold">Not Found</h2>
+      <Frown className="h-20 w-20" />
+      <h2 className="text-2xl font-bold">404 Not Found</h2>
       <p>Could not find requested resource</p>
       <Button asChild variant="secondary">
         <Link href="/">Return Home</Link>
