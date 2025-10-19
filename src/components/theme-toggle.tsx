a3ed93c11e6dd08e67bca032374290a60b8c88e6
jesus <149453045+jesus-b32@users.noreply.github.com>
2025-10-19 12:26:50 -0700

    updated dropdown menu trigger button to only render icon when theme is being used.

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -19,14 +19,11 @@ export function ThemeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun
-            className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-            data-testid="sun-icon"
-          />
-          <Moon
-            className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-            data-testid="moon-icon"
-          />
+          {theme === "dark" ? (
+            <Moon className="h-[1.2rem] w-[1.2rem]" data-testid="moon-icon" />
+          ) : (
+            <Sun className="h-[1.2rem] w-[1.2rem]" data-testid="sun-icon" />
+          )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
